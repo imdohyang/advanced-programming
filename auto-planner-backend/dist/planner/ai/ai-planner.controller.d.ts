8baeececd3843d53a54f5f6c4efe32ddc49501c3
dohyangim <dohyangim@naver.com>
2025-05-20 10:39:48 +0900

    .gitignore

--- auto-planner-backend/dist/planner/ai/ai-planner.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AiPlannerService } from './ai-planner.service';
-export declare class AiPlannerController {
-    private readonly aiPlannerService;
-    constructor(aiPlannerService: AiPlannerService);
-    generatePlan(body: {
-        userId: string;
-    }): Promise<any>;
-}
