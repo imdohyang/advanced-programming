2a610cf1120548439432ba2c00c61caee4e09d2f
WJYEE <wowoca7@gmail.com>
2025-06-05 00:44:53 +0900

    fix: update planner module again

--- auto-planner-backend/src/planner/planner.controller.ts
+++ auto-planner-backend/src/planner/planner.controller.ts
@@ -1,32 +1,32 @@
-// // planner.controller.ts
-// import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
-// import { PlannerService } from './planner.service';
-// import { ConfirmPlanDto } from './dto/confirm-plan.dto';
-// import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-// import { ApiBearerAuth, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
-// import { getToken } from 'src/auth/notion-token.store';
+// planner.controller.ts
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { PlannerService } from './planner.service';
+import { ConfirmPlanDto } from './dto/confirm-plan.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { getToken } from 'src/auth/notion-token.store';
 
-// @ApiTags('planner')
-// @Controller('planner')
-// export class PlannerController {
-//   constructor(private readonly plannerService: PlannerService) {}
+@ApiTags('planner')
+@Controller('planner')
+export class PlannerController {
+  constructor(private readonly plannerService: PlannerService) {}
 
-//   @Post(':id/confirm')
-//   @UseGuards(JwtAuthGuard)
-//   @ApiBearerAuth('access-token')
-//   @ApiParam({ name: 'id', description: '확정할 계획 ID' })
-//   @ApiOperation({ summary: '공부 계획 확정 및 Notion 연동 (mock)' })
-//   confirmPlan(
-//     @Param('id') id: string,
-//     @Body() dto: ConfirmPlanDto,
-//   ) {
-//     const token = getToken(dto.userId); // ✅ Notion token 불러오기
-//     // console.log(`[PLANNER] 불러온 Notion token: ${token}`);
-//     console.log(`[DEBUG] Notion token for user ${dto.userId}:`, token);
+  @Post(':id/confirm')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  @ApiParam({ name: 'id', description: '확정할 계획 ID' })
+  @ApiOperation({ summary: '공부 계획 확정 및 Notion 연동 (mock)' })
+  confirmPlan(
+    @Param('id') id: string,
+    @Body() dto: ConfirmPlanDto,
+  ) {
+    const token = getToken(dto.userId); // ✅ Notion token 불러오기
+    // console.log(`[PLANNER] 불러온 Notion token: ${token}`);
+    console.log(`[DEBUG] Notion token for user ${dto.userId}:`, token);
 
 
-//     return this.plannerService.confirmPlan(id, dto);
-//   }
-// }
+    return this.plannerService.confirmPlan(id, dto);
+  }
+}
 
 
